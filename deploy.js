95e0915ce5d473a9a06f7152a4048b35b4f45645
Michael Joyner <joyner.michael@gmail.com>
2017-05-03 09:43:44 +0800

    update deploy script to use correct bucket

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const exec = require('child_process').exec;
-const cmd = `./node_modules/s3-deploy/.bin/s3-deploy './dist/**' --cwd './dist/' --region ap-southeast-1 --bucket runhappy`;
+const cmd = `./node_modules/s3-deploy/.bin/s3-deploy './dist/**' --cwd './dist/' --region ap-southeast-1 --bucket runwithlowly.com`;
 
 exec(cmd, function(error, stdout, stderr) {
   console.log(stdout);
